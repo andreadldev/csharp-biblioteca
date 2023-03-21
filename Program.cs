2f3e4fee8b4aa56b5c4901b068f7a7e642c2a1fc
andreadldev <andrea.delisi2@gmail.com>
2023-03-21 15:27:41 +0100

    Registrazione utente

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_biblioteca;
+using System.Runtime.ConstrainedExecution;
+
+Console.WriteLine("Gestionale biblioteca");
+bool registered = false;
+
+while (true)
+{
+    Console.WriteLine("registrati | esci");
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "registrati":
+            if (registered == true)
+            {
+                Console.WriteLine("Sei già registrato.");
+                break;
+            }
+            Register();
+            break;
+        case "esci" or "exit":
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Input non valido");
+            break;
+    }
+}
+
+void Register()
+{
+    Console.WriteLine("\nInserisci il nome");
+    var first_name = Console.ReadLine();
+
+    Console.WriteLine("\nInserisci il cognome");
+    var last_name = Console.ReadLine();
+
+    Console.WriteLine("\nInserisci l'email");
+    var email = Console.ReadLine();
+
+    Console.WriteLine("\nInserisci la password");
+    var password = Console.ReadLine();
+
+    Console.WriteLine("\nInserisci il numero");
+    var number = Console.ReadLine();
+
+    var user = new User(first_name, last_name, email, password, number);
+    Console.WriteLine($"\nNuovo utente registrato: " +
+        $"\n{user.first_name} {user.last_name}" +
+        $"\n{user.email}" +
+        $"\n{user.number}");
+    registered = true;
+}
---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class User
+    {
+        public int id { get; } = 1;
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string number { get; set; }
+
+        public User(string first_name, string last_name, string email, string password, string number)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.email = email;
+            this.password = password;
+            this.number = number;
+        }
+    }
+}
